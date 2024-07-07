3bde45c40a8b17e497f398f8b885338200a7ef46
unknown <shanzidadisha1234@gmail.com>
2024-07-07 13:22:36 -0700

    Auth Context Added

---
+++ AuthContext.js
@@ -0,0 +1,66 @@
+import React, { createContext, useState, useEffect } from "react";
+import { auth } from "./firebase.config";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+const AuthContext = createContext({
+  currentUser: null,
+  signIn: () => {},
+  signUp: () => {},
+  signOut: () => {},
+});
+
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const signUp = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setCurrentUser(userCredential.user);
+      return userCredential;
+    } catch (error) {
+      console.error("Error during sign up:", error);
+    }
+  };
+
+  const signIn = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setCurrentUser(userCredential.user);
+    } catch (error) {
+      console.error("Error during sign in:", error);
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await auth.signOut();
+      setCurrentUser(null);
+    } catch (error) {
+      console.error("Error during sign out:", error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ currentUser, signIn, signOut, signUp }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
