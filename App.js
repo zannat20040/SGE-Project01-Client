13e9e0f75e70c85e2015b126fe72d8e5a96b6796
unknown <shanzidadisha1234@gmail.com>
2024-07-07 12:58:37 -0700

    signup page added

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, PaperProvider } from "react-native-paper";
 import {
   Raleway_100Thin,
   Raleway_200ExtraLight,
@@ -13,11 +13,16 @@ import {
   Raleway_900Black,
 } from "@expo-google-fonts/raleway";
 import { useFonts } from "expo-font";
-import Login from "./src/Layout/Login";
 import Signup from "./src/Layout/Signup";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/Layout/Login";
+import Root from "./src/Layout/Root";
 
+const Stack = createStackNavigator();
 
 export default function App() {
+
 
   const [fontsLoaded] = useFonts({
     Raleway_100Thin,
@@ -32,17 +37,32 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return <Text>Loading...</Text>;
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator animating={true} color="#7367f0" size="medium" />
+      </View>
+    );
   }
 
-
   return (
-     <PaperProvider>
-      <View style={styles.container}>
-        {/* <Login /> */}
-        <Signup />
-        <StatusBar style="auto" />
-      </View>
+    <PaperProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" backgroundColor="white" />
+        <NavigationContainer>
+          <AuthProvider>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false, // Hide the header
+              }}
+              initialRouteName={user ? "Root" : "Login"}
+            >
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Signup" component={Signup} />
+              <Stack.Screen name="Root" component={Root} />
+            </Stack.Navigator>
+          </AuthProvider>
+        </NavigationContainer>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
@@ -50,8 +70,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
-    paddingVertical: 30,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mainContainer: {
+    flex: 1,
     backgroundColor: "white",
   },
 });
